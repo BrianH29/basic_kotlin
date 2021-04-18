0bc90e7e13028561dbbe8c4d5eb8c1717d2b4aea
brian <brianhwang29@gmail.com>
2021-04-18 15:27:53 +0900

    extension plus string

--- app/src/main/java/com/bh/hellokotlin/inheritance/extension.kt
+++ app/src/main/java/com/bh/hellokotlin/inheritance/extension.kt
@@ -1,2 +1,26 @@
 package com.bh.hellokotlin.inheritance
 
+
+fun main(){
+    textStringExtension()
+}
+
+fun textStringExtension(){
+    var original = "Hello"
+    var added = "Guys~~"
+
+    println("${original.plus(added)}")
+}
+
+fun String.plus(word:String):String{
+    return this+word
+}
+
+fun testExtension(){
+    val name = "Brian"
+    val birth = "2000.01.01"
+    val age = 30
+    val addr = "Sungnam Korea"
+
+    println("")
+}
