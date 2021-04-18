323ffb94434ed2885f4804a64fb7c2138adc57a3
brian <brianhwang29@gmail.com>
2021-04-18 15:26:51 +0900

    inheritance parent class

---
+++ app/src/main/java/com/bh/hellokotlin/inheritance/extension.kt
@@ -0,0 +1,2 @@
+package com.bh.hellokotlin.inheritance
+
--- app/src/main/java/com/bh/hellokotlin/inheritance/parentClass.kt
+++ app/src/main/java/com/bh/hellokotlin/inheritance/parentClass.kt
@@ -8,3 +8,32 @@ open class User(){
         println("My name is $name")
     }
 }
+
+//Override
+open class BaseClass2(){
+    open var opened: String = "I am"
+}
+
+//abstract
+abstract class Animal{
+    fun walk(){
+        println("is walking")
+    }
+
+    abstract fun move()
+}
+
+//interface
+interface InterfaceKotlin{
+    var name : String
+    fun getAddress()
+    fun setAddress()
+}
+
+//visibility modifiers
+open class Top{
+    private val privateVal = 1
+    protected open val protectedVal = 2
+    internal val internalVal = 3
+    val defaulVal = 4
+}
